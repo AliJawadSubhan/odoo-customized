c3eb43cfec30d4a8462e6ab66d357b3f0e84179b
Ali Jawad <jawadsubhan7@gmail.com>
2026-08-22 18:03:55 +0500

    ZATCA QR

--- addons/markition_zatca_qr/__manifest__.py
+++ addons/markition_zatca_qr/__manifest__.py
@@ -3,9 +3,11 @@
     'version': '19.0.1.0.0',
     'summary': 'Ensures the ZATCA QR code appears on printed/exported Saudi tax invoice PDFs',
     'author': 'Markition',
-    'depends': ['l10n_sa_edi'],
+    'depends': ['l10n_sa', 'l10n_sa_edi'],
     'installable': True,
     'auto_install': False,
     'license': 'LGPL-3',
-    'data': [],
+    'data': [
+        'views/report_invoice.xml',
+    ],
 }
